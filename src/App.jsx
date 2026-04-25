a724f6569e0c573c557bb58cad00772c21eb0f7b
Yaroslav Kovalchuk <yaroslavkovalc03@gmail.com>
2026-04-25 11:58:43 +0300

    feat: add tailwind and shadcn (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
-  return (
-    <div className="text">Pohoda</div>
-  )
+  return <div className='text'>Pohoda</div>;
 }
 
-export default App
+export default App;
