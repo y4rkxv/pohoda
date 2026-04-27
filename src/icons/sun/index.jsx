4380299519877df434392ded1071066d8f680032
Yaroslav <yaroslavkovalc03@gmail.com>
2026-04-27 16:39:43 +0300

    refactor: use CSS variables for header colors, extract SunIcon component, add constants

---
+++ src/icons/sun/index.jsx
@@ -0,0 +1,8 @@
+import { DEFAULT_ICON_SIZE } from '@/constants';
+import { CloudSun } from 'lucide-react';
+
+const Sun = () => {
+  return <CloudSun size={DEFAULT_ICON_SIZE} className='text-orange-500 mr-2' />;
+};
+
+export default Sun;
