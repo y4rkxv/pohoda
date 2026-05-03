0a0e360f7c9b5e69abbef6ab1701aa28433ab0eb
Yaroslav <yaroslavkovalc03@gmail.com>
2026-05-03 11:45:09 +0300

    refactor: move api files into separate folders

--- src/hooks/use-weather/index.js
+++ src/hooks/use-weather/index.js
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { getWeather } from '@/api';
+import { getWeather } from '@/api/get-weather';
 
 export const useWeather = city => {
   return useQuery({
